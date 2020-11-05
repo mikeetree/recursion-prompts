6a8a991df7b9c0801fe4224e93bf6dda038c81cd
Your Name <mikeetree@gmail.com>
2020-11-05 15:10:37 -0500

    Implemented sum

--- src/recursion.js
+++ src/recursion.js
@@ -13,6 +13,7 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+    return array.length > 0 ? array[0] + sum(array.slice(1)) : 0;
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
